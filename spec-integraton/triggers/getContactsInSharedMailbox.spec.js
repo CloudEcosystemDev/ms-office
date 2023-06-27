0e3500a12333ed31cbf7db9f5974fd24449fa481
Olha Virolainen <virolainenolha@gmail.com>
2023-06-27 13:00:39 +0300

    use logger with context (#11)

---
+++ spec-integraton/triggers/getContactsInSharedMailbox.spec.js
@@ -0,0 +1,19 @@
+const { getContext, getAccessToken } = require("../common");
+const { expect } = require("chai");
+const trigger = require("../../lib/triggers/getContactsInSharedMailbox");
+
+describe("Trigger getContactsInSharedMailbox test", () => {
+  it("should return data", async () => {
+    const msg = { attachments: {}, data: {}, metadata: {}, headers: {} };
+    const cfg = {
+      "nodeSettings":{},
+      "accessToken": await getAccessToken(),
+      mailboxId: 'shared@FlowMateDemo.onmicrosoft.com'
+    };
+    const snapshot = { };
+    const ctx = getContext();
+    await trigger.process.call(ctx, msg, cfg, snapshot);
+    expect(ctx.emit.callCount).to.be.equal(34);
+    expect(ctx.emit.args[0][0]).to.be.equal('data');
+  });
+});
